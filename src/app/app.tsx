4662738a473f668a226a0ec80a80f8a6cf4c663f
Eccentric <eccentric@retrac.site>
2024-03-04 21:49:43 +0000

    Add gh action

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from "react";
+import { LOADING_STATES, useConfigControl } from "src/state/config";
+
 import { RouterProvider } from "@tanstack/react-router";
 import router from "src/app/router";
 
@@ -10,9 +12,12 @@ import "src/styles/defaults.css";
 
 const App = () => {
   const handleContextMenu = (e: Event) => e.preventDefault();
+  const config = useConfigControl();
 
   useEffect(() => {
     window.addEventListener("contextmenu", handleContextMenu);
+    config.set_loader("launcher", LOADING_STATES.AWAITING_ACTION);
+
     return () => window.removeEventListener("contextmenu", handleContextMenu);
   }, []);
 
